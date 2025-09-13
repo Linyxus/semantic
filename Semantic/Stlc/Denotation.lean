848c163491eacc49f572ac29f32bcc7ae3e3346f
Yichen Xu <yichen.x@outlook.com>
2025-09-13 22:48:11 +0200

    New definitions of type denotation

--- Semantic/Stlc/Denotation.lean
+++ Semantic/Stlc/Denotation.lean
@@ -1,35 +1,30 @@
 import Semantic.Stlc.TypeSystem
-import Semantic.Stlc.Interpreter
+import Semantic.Stlc.BigStep
 import Mathlib.Tactic
 
 mutual
 
-def Ty.val_denot : Ty -> ∀ {n}, Config n -> Prop
-| .bool => fun config =>
-  config.e.is_bool_val
-| .nat => fun config =>
-  config.e.is_num_val
-| .arrow T U => fun config =>
+def Ty.val_denot : Ty -> Exp 0 -> Prop
+| .bool => fun e =>
+  e.IsBoolVal
+| .nat => fun e  =>
+  e.IsNumVal
+| .arrow T U => fun e =>
   ∀ arg_v,
-    Ty.val_denot T ⟨config.s, arg_v⟩ ->
-    Ty.exp_denot U ⟨config.s, .app config.e arg_v⟩
+    Ty.val_denot T arg_v ->
+    Ty.exp_denot U (.app e arg_v)
 
-def Ty.exp_denot : Ty -> ∀ {n}, Config n -> Prop
-| T => fun config =>
+def Ty.exp_denot : Ty -> Exp 0 -> Prop
+| T => fun e =>
   ∃ v,
-    Eval config.s config.e v
-    ∧ Ty.val_denot T ⟨config.s, v⟩
+    Eval e v
+    ∧ Ty.val_denot T v
 
 end
 
-def TypedStore : Store n -> Ctx n -> Prop
-| .empty, .empty => True
-| .val s v, .var Γ T =>
-  TypedStore s Γ ∧ Ty.val_denot T ⟨s, v⟩
+-- def SemanticTyping (Γ : Ctx n) (e : Exp n) (T : Ty) : Prop :=
+--   ∀ s,
+--     TypedStore s Γ ->
+--     Ty.exp_denot T ⟨s, e⟩
 
-def SemanticTyping (Γ : Ctx n) (e : Exp n) (T : Ty) : Prop :=
-  ∀ s,
-    TypedStore s Γ ->
-    Ty.exp_denot T ⟨s, e⟩
-
-notation:65 Γ " ⊨ " e " : " T => SemanticTyping Γ e T
+-- notation:65 Γ " ⊨ " e " : " T => SemanticTyping Γ e T
