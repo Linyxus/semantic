ac1075b77b3a4ef131dc9df6b5d0e5191fa4d855
Yichen Xu <yichen.x@outlook.com>
2025-09-15 21:47:04 +0200

    Small step

---
+++ Semantic/Stlc/SmallStep/Denotation.lean
@@ -0,0 +1,38 @@
+import Semantic.Stlc.TypeSystem
+import Semantic.Stlc.SmallStep.Eval
+import Semantic.Stlc.Syntax
+import Semantic.Stlc.Substitution
+import Mathlib.Tactic
+
+mutual
+
+def Ty.val_denot : Ty -> Exp 0 -> Prop
+| .bool => fun e =>
+  e.IsBoolVal
+| .nat => fun e  =>
+  e.IsNumVal
+| .arrow T U => fun e =>
+  e.IsAbsVal ∧
+  ∀ arg_v,
+    Ty.val_denot T arg_v ->
+    Ty.exp_denot U (.app e arg_v)
+
+def Ty.exp_denot : Ty -> Exp 0 -> Prop
+| T => fun e =>
+  ∃ v,
+    Reduce e v
+    ∧ Ty.val_denot T v
+
+end
+
+def TypedStore : Store n -> Ctx n -> Prop
+| .nil, .empty => True
+| .cons v _ s, .var Γ T =>
+  Ty.val_denot T v ∧ TypedStore s Γ
+
+def SemanticTyping (Γ : Ctx n) (e : Exp n) (T : Ty) : Prop :=
+  ∀ s,
+    TypedStore s Γ ->
+    Ty.exp_denot T (e.subst (Subst.fromStore s))
+
+notation:65 Γ " ⊨ " e " : " T => SemanticTyping Γ e T
