49385e8ec9e0ce5090aaa4298f51ba6a994d2edb
Yichen Xu <yichen.x@outlook.com>
2025-09-17 17:05:03 +0200

    Wip

--- Semantic/Fsub/Denotation.lean
+++ Semantic/Fsub/Denotation.lean
@@ -4,11 +4,25 @@ namespace Fsub
 
 mutual
 
+-- Termination checking fails
 def Ty.val_denot : Ty {} -> Store -> Exp {} -> Prop
 | .top => fun s e => True
-| .singleton x => fun s e => e = .var x
-| .arrow T U => fun s e => sorry
+| .singleton x => fun s e => False  -- A value cannot be a singleton
+| .arrow T U => fun s e =>
+  ∃ T0 e0,
+    e = .abs T0 e0 ∧
+    (∀ x,
+      Ty.var_denot T s x ->
+      Ty.val_denot (U.subst (Subst.openVar x)) s (e0.subst (Subst.openVar x)))
 | .poly T U => fun s e => sorry
+
+def Ty.var_denot : Ty {} -> Store -> Var {} -> Prop
+| .singleton x => fun s e => e = x
+| other => fun s e =>
+  ∃ fx v hv,
+    e = .free fx ∧
+    s.lookup fx = some ⟨v, hv⟩ ∧
+    Ty.val_denot other s v
 
 def Ty.exp_denot : Ty {} -> Store -> Exp {} -> Prop :=
   fun T s e =>
