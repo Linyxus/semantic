f4050391feea204a9821fd558998b1b0e7dbfd26
Yichen Xu <yichen.x@outlook.com>
2025-09-25 17:46:17 +0200

    Finish retyping theory

--- Semantic/Fsub/Denotation/Retype.lean
+++ Semantic/Fsub/Denotation/Retype.lean
@@ -73,11 +73,104 @@ mutual
 
 theorem retype_val_denot
   (ρ : Retype env1 σ env2) :
-  Ty.val_denot env1 T ≈ Ty.val_denot env2 (T.subst σ) := sorry
+  Ty.val_denot env1 T ≈ Ty.val_denot env2 (T.subst σ) :=
+  match T with
+  | .top => by
+    apply Denot.eq_to_equiv
+    simp [Ty.val_denot, Ty.subst]
+  | .tvar X => by
+    simp [Ty.val_denot, Ty.subst]
+    exact ρ.tvar X
+  | .singleton x => by
+    simp [Ty.val_denot, Ty.subst]
+    intro s e
+    simp
+    cases x with
+    | bound x =>
+      conv =>
+        arg 1
+        simp [Var.subst, interp_var]
+      have := ρ.var x
+      simp [this]
+      aesop
+    | free n =>
+      simp [Var.subst, interp_var]
+  | .arrow T1 T2 => by
+    have ih1 := retype_val_denot ρ (T:=T1)
+    simp [Ty.val_denot, Ty.subst]
+    intro s0 e0
+    simp; constructor
+    next =>
+      intro h
+      have ⟨T0, body, hr, hd⟩ := h
+      use T0, body
+      apply And.intro hr
+      intro arg harg
+      have ih2 := retype_exp_denot (ρ.liftVar (x:=arg)) (T:=T2)
+      have harg' := (ih1 _ _).mpr harg
+      specialize hd _ harg'
+      have hd' := (ih2 _ _).mp hd
+      exact hd'
+    next =>
+      intro h
+      have ⟨T0, body, hr, hd⟩ := h
+      use T0, body
+      apply And.intro hr
+      intro arg harg
+      have ih2 := retype_exp_denot (ρ.liftVar (x:=arg)) (T:=T2)
+      have harg' := (ih1 _ _).mp harg
+      specialize hd _ harg'
+      have hd' := (ih2 _ _).mpr hd
+      exact hd'
+  | .poly T1 T2 => by
+    have ih1 := retype_val_denot ρ (T:=T1)
+    simp [Ty.val_denot, Ty.subst]
+    intro s0 e0; simp
+    constructor
+    next =>
+      intro h
+      have ⟨T0, e0, hr, hd⟩ := h
+      use T0, e0
+      apply And.intro hr
+      intro denot himply
+      have ih2 := retype_exp_denot (ρ.liftTVar (d:=denot)) (T:=T2)
+      have himply' : denot.Imply (Ty.val_denot env1 T1) := by
+        intro s e hdenot
+        have := (ih1 s e).mpr (himply s e hdenot)
+        exact this
+      specialize hd denot himply'
+      have hd' := (ih2 s0 (e0.subst (Subst.openTVar .top))).mp hd
+      exact hd'
+    next =>
+      intro h
+      have ⟨T0, e0, hr, hd⟩ := h
+      use T0, e0
+      apply And.intro hr
+      intro denot himply
+      have ih2 := retype_exp_denot (ρ.liftTVar (d:=denot)) (T:=T2)
+      have himply' : denot.Imply (Ty.val_denot env2 (T1.subst σ)) := by
+        intro s e hdenot
+        have := (ih1 s e).mp (himply s e hdenot)
+        exact this
+      specialize hd denot himply'
+      have hd' := (ih2 s0 (e0.subst (Subst.openTVar .top))).mpr hd
+      exact hd'
 
 theorem retype_exp_denot
   (ρ : Retype env1 σ env2) :
-  Ty.exp_denot env1 T ≈ Ty.exp_denot env2 (T.subst σ) := sorry
+  Ty.exp_denot env1 T ≈ Ty.exp_denot env2 (T.subst σ) := by
+  have ih := retype_val_denot ρ (T:=T)
+  intro s e
+  simp [Ty.exp_denot]
+  constructor
+  · intro h
+    have ⟨s', v', hr, hv⟩ := h
+    have := ih s' v'
+    aesop
+  · intro h
+    have ⟨s', v', hr, hv⟩ := h
+    have := ih s' v'
+    aesop
 
 end
 
