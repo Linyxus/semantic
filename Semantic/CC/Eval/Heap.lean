508e2eea1f6caeca6789d277509215669e725d4f
Yichen Xu <yichen.x@outlook.com>
2025-10-28 23:12:12 +0100

    Define other `WfInHeap` predicates

--- Semantic/CC/Eval/Heap.lean
+++ Semantic/CC/Eval/Heap.lean
@@ -141,15 +141,114 @@ theorem Heap.extend_subsumes {H : Heap} {l : Nat}
     contradiction
   next => exact hlookup
 
-inductive CaptureSet.WfInHeap : CaptureSet {} -> Heap -> Prop where
+inductive CaptureSet.WfInHeap : CaptureSet s -> Heap -> Prop where
 | wf_empty :
   CaptureSet.WfInHeap {} H
 | wf_union :
   CaptureSet.WfInHeap C1 H ->
   CaptureSet.WfInHeap C2 H ->
   CaptureSet.WfInHeap (C1 ∪ C2) H
-| wf_var :
+| wf_var_free :
   H x = some val ->
   CaptureSet.WfInHeap (CaptureSet.var (.free x)) H
+| wf_var_bound :
+  CaptureSet.WfInHeap (CaptureSet.var (.bound x)) H
+| wf_cvar :
+  CaptureSet.WfInHeap (CaptureSet.cvar x) H
+
+inductive Var.WfInHeap : Var k s -> Heap -> Prop where
+| wf_bound :
+  Var.WfInHeap (.bound x) H
+| wf_free :
+  H n = some val ->
+  Var.WfInHeap (.free n) H
+
+inductive CaptureBound.WfInHeap : CaptureBound s -> Heap -> Prop where
+| wf_unbound :
+  CaptureBound.WfInHeap .unbound H
+| wf_bound :
+  CaptureSet.WfInHeap cs H ->
+  CaptureBound.WfInHeap (.bound cs) H
+
+inductive Ty.WfInHeap : Ty sort s -> Heap -> Prop where
+-- Shape types
+| wf_top :
+  Ty.WfInHeap .top H
+| wf_tvar :
+  Ty.WfInHeap (.tvar x) H
+| wf_arrow :
+  Ty.WfInHeap T1 H ->
+  Ty.WfInHeap T2 H ->
+  Ty.WfInHeap (.arrow T1 T2) H
+| wf_poly :
+  Ty.WfInHeap T1 H ->
+  Ty.WfInHeap T2 H ->
+  Ty.WfInHeap (.poly T1 T2) H
+| wf_cpoly :
+  CaptureBound.WfInHeap cb H ->
+  Ty.WfInHeap T H ->
+  Ty.WfInHeap (.cpoly cb T) H
+| wf_unit :
+  Ty.WfInHeap .unit H
+| wf_cap :
+  Ty.WfInHeap .cap H
+-- Capturing types
+| wf_capt :
+  CaptureSet.WfInHeap cs H ->
+  Ty.WfInHeap T H ->
+  Ty.WfInHeap (.capt cs T) H
+-- Existential types
+| wf_exi :
+  Ty.WfInHeap T H ->
+  Ty.WfInHeap (.exi T) H
+| wf_typ :
+  Ty.WfInHeap T H ->
+  Ty.WfInHeap (.typ T) H
+
+inductive Exp.WfInHeap : Exp s -> Heap -> Prop where
+| wf_var :
+  Var.WfInHeap x H ->
+  Exp.WfInHeap (.var x) H
+| wf_abs :
+  CaptureSet.WfInHeap cs H ->
+  Ty.WfInHeap T H ->
+  Exp.WfInHeap e H ->
+  Exp.WfInHeap (.abs cs T e) H
+| wf_tabs :
+  CaptureSet.WfInHeap cs H ->
+  Ty.WfInHeap T H ->
+  Exp.WfInHeap e H ->
+  Exp.WfInHeap (.tabs cs T e) H
+| wf_cabs :
+  CaptureSet.WfInHeap cs H ->
+  CaptureBound.WfInHeap cb H ->
+  Exp.WfInHeap e H ->
+  Exp.WfInHeap (.cabs cs cb e) H
+| wf_pack :
+  CaptureSet.WfInHeap cs H ->
+  Var.WfInHeap x H ->
+  Exp.WfInHeap (.pack cs x) H
+| wf_app :
+  Var.WfInHeap x H ->
+  Var.WfInHeap y H ->
+  Exp.WfInHeap (.app x y) H
+| wf_tapp :
+  Var.WfInHeap x H ->
+  Ty.WfInHeap T H ->
+  Exp.WfInHeap (.tapp x T) H
+| wf_capp :
+  Var.WfInHeap x H ->
+  CaptureSet.WfInHeap cs H ->
+  Exp.WfInHeap (.capp x cs) H
+| wf_letin :
+  Exp.WfInHeap e1 H ->
+  Exp.WfInHeap e2 H ->
+  Exp.WfInHeap (.letin e1 e2) H
+| wf_unpack :
+  Exp.WfInHeap e1 H ->
+  Exp.WfInHeap e2 H ->
+  Exp.WfInHeap (.unpack e1 e2) H
+| wf_unit :
+  Exp.WfInHeap .unit H
 
 end CC
