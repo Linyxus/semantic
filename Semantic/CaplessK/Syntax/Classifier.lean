7e497124bda13e886f1603fb7642a5a895cd293d
Yichen Xu <yichen.x@outlook.com>
2026-01-21 23:45:43 +0100

    Port the rest of Classifier module

--- Semantic/CaplessK/Syntax/Classifier.lean
+++ Semantic/CaplessK/Syntax/Classifier.lean
@@ -1,4 +1,11 @@
 import Semantic.CaplessK.Syntax.Classifier.Core
+import Semantic.CaplessK.Syntax.Classifier.Kind
+import Semantic.CaplessK.Syntax.Classifier.Subtract
+import Semantic.CaplessK.Syntax.Classifier.Intersection
+import Semantic.CaplessK.Syntax.Classifier.Semantics
+import Semantic.CaplessK.Syntax.Classifier.Subkind
+import Semantic.CaplessK.Syntax.Classifier.Disjoint
+import Semantic.CaplessK.Syntax.Classifier.Basic
 
 /-!
 Classifier definitions for CaplessK.
