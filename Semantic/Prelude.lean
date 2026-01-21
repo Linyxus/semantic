c6b90f3da33dc7fe4442b406cd031dfff0e654b5
Yichen Xu <yichen.x@outlook.com>
2026-01-21 22:02:23 +0100

    Add classifiers

--- Semantic/Prelude.lean
+++ Semantic/Prelude.lean
@@ -7,3 +7,10 @@ class HasExpDenotation (T : Type) (Env : Type) (Denot : outParam Type) where
   interp : Env -> T -> Denot
 
 notation:max "⟦" T "⟧e_[" ρ "]" => HasExpDenotation.interp ρ T
+
+/-- `easy` is an alias of `assumption`. -/
+macro "easy" : tactic => `(tactic| assumption)
+/-- `apply!` applies a hypothesis and `easy` on all generated subgoals. -/
+macro "apply!" e:term : tactic => `(tactic| apply $e <;> easy)
+/-- `apply?` applies a hypothesis and try to `easy` on all generated subgoals. -/
+macro "apply?" e:term : tactic => `(tactic| apply $e <;> try easy)
