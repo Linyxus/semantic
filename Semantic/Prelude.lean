153de2cbd2c35e1b6e1b63083a4a47a2878c441d
Yichen Xu <yichen.x@outlook.com>
2026-01-29 20:22:41 +0100

    Define finmap in Prelude.lean

--- Semantic/Prelude.lean
+++ Semantic/Prelude.lean
@@ -1,3 +1,5 @@
+import Mathlib.Data.Finset.Basic
+
 class HasDenotation (T : Type) (Env : Type) (Denot : outParam Type) where
   interp : Env -> T -> Denot
 
@@ -14,3 +16,14 @@ macro "easy" : tactic => `(tactic| assumption)
 macro "apply!" e:term : tactic => `(tactic| apply $e <;> easy)
 /-- `apply?` applies a hypothesis and try to `easy` on all generated subgoals. -/
 macro "apply?" e:term : tactic => `(tactic| apply $e <;> try easy)
+
+structure Finmap (K V : Type) where
+  apply : K -> Option V
+  dom : Finset K
+  hasdom : ∀ k, k ∈ dom ↔ ∃ v, apply k = some v
+
+namespace Finmap
+
+def extend (m : Finmap a b) (k : a) (v : b) : Finmap a b := sorry
+
+end Finmap
