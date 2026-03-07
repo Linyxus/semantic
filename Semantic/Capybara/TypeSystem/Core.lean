8b2d8bed44bcbc050a1bfab1c78c72e5df88c8c2
Yichen Xu <yichen.x@outlook.com>
2026-03-07 14:13:18 +0800

    Update the type system

--- Semantic/Capybara/TypeSystem/Core.lean
+++ Semantic/Capybara/TypeSystem/Core.lean
@@ -121,7 +121,7 @@ inductive HasType : CaptureSet s -> Ctx s -> Exp s -> Ty .exi s -> Prop where
   Γ.LookupVar x T ->
   ----------------------------
   HasType
-    (.var .epsilon (.bound x))
+    {}
     Γ
     (.var (.bound x))
     (.typ (T.refineCaptureSet (.var .epsilon (.bound x))))
@@ -130,13 +130,13 @@ inductive HasType : CaptureSet s -> Ctx s -> Exp s -> Ty .exi s -> Prop where
   Γ.LookupVar x (.cell C) ->
   ---------------------------------
   HasType
-    (.var .ro (.bound x))
+    {}
     Γ
     (.reader (.bound x))
     (.typ (.reader (.var .ro (.bound x))))
 | abs {T1 : Ty .capt s} :
   T1.IsClosed ->
-  HasType (cs.rename Rename.succ ∪ (.var .epsilon (.bound .here))) (Γ,x:T1) e T2 ->
+  HasType (cs.rename Rename.succ) (Γ,x:T1) e T2 ->
   ----------------------------
   HasType {} Γ (.abs cs T1 e) (.typ (.arrow T1 cs T2))
 | tabs {S : PureTy s} :
@@ -150,23 +150,23 @@ inductive HasType : CaptureSet s -> Ctx s -> Exp s -> Ty .exi s -> Prop where
   HasType {} Γ (.cabs cs m e) (.typ (.cpoly m cs T))
 | pack {C : CaptureSet s} :
   C.IsClosed ->
-  HasType (.var .epsilon x) Γ (.var x) (.typ (T.subst (Subst.openCVar C))) ->
+  HasType {} Γ (.var x) (.typ (T.subst (Subst.openCVar C))) ->
   ----------------------------
-  HasType (.var .epsilon x) Γ (.pack C x) (.exi T)
+  HasType {} Γ (.pack C x) (.exi T)
 | app :
-  HasType (.var .epsilon x) Γ (.var x) (.typ (.arrow T1 (.var .epsilon x) T2)) ->
-  HasType (.var .epsilon y) Γ (.var y) (.typ T1) ->
+  HasType {} Γ (.var x) (.typ (.arrow T1 (.var .epsilon x) T2)) ->
+  HasType {} Γ (.var y) (.typ T1) ->
   ----------------------------
-  HasType ((.var .epsilon x) ∪ (.var .epsilon y)) Γ (.app x y) (T2.subst (Subst.openVar y))
+  HasType (.var .epsilon x) Γ (.app x y) (T2.subst (Subst.openVar y))
 | tapp {S : PureTy s} :
   S.IsClosed ->
-  HasType (.var .epsilon x) Γ (.var x) (.typ (.poly S.core (.var .epsilon x) T)) ->
+  HasType {} Γ (.var x) (.typ (.poly S.core (.var .epsilon x) T)) ->
   ----------------------------
   HasType (.var .epsilon x) Γ (.tapp x S) (T.subst (Subst.openTVar S))
 | capp {D : CaptureSet s} {m : Mutability} {I : CaptureSet s} :
   D.IsClosed ->
   HasKind Γ D m ->
-  HasType (.var .epsilon x) Γ (.var x) (.typ (.cpoly m (.var .epsilon x) T)) ->
+  HasType {} Γ (.var x) (.typ (.cpoly m (.var .epsilon x) T)) ->
   (Ty.cpoly m (.var .epsilon x) T).interfere_set = some I ->
   SepCheck Γ D I ->
   ----------------------------
@@ -195,14 +195,14 @@ inductive HasType : CaptureSet s -> Ctx s -> Exp s -> Ty .exi s -> Prop where
   ----------------------------
   HasType {} Γ (.bfalse) (.typ .bool)
 | read :
-  HasType (.var .epsilon x) Γ (.var x) (.typ (.reader C)) ->
+  HasType {} Γ (.var x) (.typ (.reader C)) ->
   ----------------------------
   HasType (.var .epsilon x) Γ (.read x) (.typ .bool)
 | write :
-  HasType (.var .epsilon x) Γ (.var x) (.typ (.cell Cx)) ->
-  HasType (.var .epsilon y) Γ (.var y) (.typ .bool) ->
+  HasType {} Γ (.var x) (.typ (.cell Cx)) ->
+  HasType {} Γ (.var y) (.typ .bool) ->
   ----------------------------
-  HasType ((.var .epsilon x) ∪ (.var .epsilon y)) Γ (.write x y) (.typ .unit)
+  HasType (.var .epsilon x) Γ (.write x y) (.typ .unit)
 | cond :
   HasType C1 Γ (.var x) (.typ .bool) ->
   HasType C2 Γ e2 T ->
@@ -216,10 +216,10 @@ inductive HasType : CaptureSet s -> Ctx s -> Exp s -> Ty .exi s -> Prop where
   ----------------------------
   HasType (C1 ∪ C2) Γ (.par e1 e2) E
 | invoke :
-  HasType (.var .epsilon x) Γ (.var x) (.typ (.cap (.var .epsilon x))) ->
-  HasType (.var .epsilon y) Γ (.var y) (.typ .unit) ->
+  HasType {} Γ (.var x) (.typ (.cap (.var .epsilon x))) ->
+  HasType {} Γ (.var y) (.typ .unit) ->
   ------------------------------------------------
-  HasType ((.var .epsilon x) ∪ (.var .epsilon y)) Γ (.app x y) (.typ .unit)
+  HasType (.var .epsilon x) Γ (.app x y) (.typ .unit)
 | subtyp :
   HasType C1 Γ e E1 ->
   Subcapt Γ C1 C2 ->
