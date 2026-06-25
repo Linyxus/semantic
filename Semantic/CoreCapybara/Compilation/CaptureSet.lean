33ca9b6d4e67736d8cee4c129d172ede32f81c65
Yichen Xu <yichen.x@outlook.com>
2026-06-25 16:04:12 +0200

    Define compilation

---
+++ Semantic/CoreCapybara/Compilation/CaptureSet.lean
@@ -0,0 +1,10 @@
+import Semantic.CoreCapybara.Compilation.CompilerCtx
+open CoreCapybara
+namespace Compilation
+
+def CaptureSet.compile
+  (ctx : CompilerCtx s1 s2)
+  (cs : CaptureSet s1) :
+  CaptureSet s2 := sorry
+
+end Compilation
